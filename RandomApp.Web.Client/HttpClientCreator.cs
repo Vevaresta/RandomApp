e3c63884f0841290bea19983c2ea42e4a0f397fc
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com>
2024-12-21 12:44:24 +0100

    Added loging in HttpClientCreator class

--- RandomApp.Web.Client/HttpClientCreator.cs
+++ RandomApp.Web.Client/HttpClientCreator.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using NLog;
 
 namespace RandomApp.Web.Client
 {
@@ -8,22 +9,33 @@ namespace RandomApp.Web.Client
         private IHttpClientFactory _httpClientFactory;
         //private ILocalStorageService _localStorage;
         private IConfiguration _configuration;
+        private ILogger _logger;
 
         public HttpClientCreator(IHttpClientFactory httpClientFactory, IConfiguration configuration)
         {
             _httpClientFactory = httpClientFactory;
             _configuration = configuration;
+            this._logger = LogManager.GetCurrentClassLogger();
+            _logger.Info("HttpClientCreator service initialized");
         }
 
         public HttpClient GetHttpClient()
         {
+            _logger.Info("Creating new HTTP client for API communication");
             var httpClient = _httpClientFactory.CreateClient("RandomApp_ApiHttpClient");
             var baseAddress = _configuration.GetValue<string>("ApiSettings:BaseAddress");
+            _logger.Debug("Configuring client with base address: {BaseAddress}",
+                baseAddress?.TrimEnd('/') ?? "null");
+
             if (string.IsNullOrWhiteSpace(baseAddress))
             {
+                _logger.Error("Base adress configuration is missing or empty");
                 throw new InvalidOperationException("Base address is not configured.");
             }
             httpClient.BaseAddress = new Uri(baseAddress);
+
+            _logger.Info("HTTP client successfully configured and ready for use");
+
             return httpClient;
         }
 
