448874c3f1066e58a91322b3d861edf922006967
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-01-23 12:21:06 +0100

    Implemented ShoppingCartUnitOfWork

---
+++ RandomApp.ShoppingCart.Infrastructure/RepositoryImplementation/ShoppingCartUnitOfWork.cs
@@ -0,0 +1,43 @@
+﻿
+using Common.Shared.Repositories;
+using RandomApp.ShoppingCart.Infrastructure.DataAccess;
+
+namespace RandomApp.ShoppingCart.Infrastructure.RepositoryImplementation
+{
+    public class ShoppingCartUnitOfWork : IUnitOfWork
+    {
+        private readonly ShoppingCartDbContext _context;
+        private bool _disposed;
+
+        public ShoppingCartUnitOfWork(ShoppingCartDbContext context)
+        {
+            _context = context;
+        }
+
+        public int Complete()
+        {
+            return _context.SaveChanges();
+        }
+
+        public async Task<int> CompleteAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposed && disposing)
+            {
+                _context.Dispose();
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
+}
