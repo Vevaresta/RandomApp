167545a38f2bee8da1c680084a9e2cafd58ae0fb
Nikola Krpan <73753308+Vevaresta@users.noreply.github.com>
2025-02-09 11:17:58 +0100

    Started Order entity

---
+++ RandomApp.OrderProcessing.Domain/Entities/Order.cs
@@ -0,0 +1,9 @@
+﻿namespace RandomApp.OrderProcessing.Domain.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+    }
+}
