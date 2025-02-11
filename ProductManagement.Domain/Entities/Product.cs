9223d60f4944439fcb03080bbeaa56ca5020e79a
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-02-11 19:21:57 +0100

    Made Price Value Object in Product/Domain layer and created domain exception

--- ProductManagement.Domain/Entities/Product.cs
+++ ProductManagement.Domain/Entities/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using RandomApp.ProductManagement.Domain.ValueObjects;
 
 
 namespace RandomApp.ProductManagement.Domain.Entities
@@ -12,7 +13,7 @@ namespace RandomApp.ProductManagement.Domain.Entities
 
         public string Name { get; set; }
 
-        public double Price { get; set; }
+        public Price Price { get; private set; }
 
         public string Category { get; set; }
         public string Description { get; set; }
---
+++ ProductManagement.Domain/ValueObjects/Price.cs
@@ -0,0 +1,22 @@
+﻿using RandomApp.ProductManagement.Domain.Exceptions;
+
+namespace RandomApp.ProductManagement.Domain.ValueObjects
+{
+    public record Price
+    {
+        public string Currency { get; private init; }
+        public decimal Amount { get; private init; }
+
+        public Price(decimal amount, string currency)
+        {
+            if (amount <= 0)
+                throw new DomainException("Price cannot be negative");
+
+            if (string.IsNullOrWhiteSpace(currency))
+                throw new DomainException("Currency is required");
+            
+            Amount = amount;
+            Currency = currency;
+        }
+    }
+}
