58114fd41ca2bc5beb699d20f4b580a381aae29e
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-02-11 20:28:46 +0100

    Added SKU value object in product sub-domain

--- ProductManagement.Domain/Entities/Product.cs
+++ ProductManagement.Domain/Entities/Product.cs
@@ -7,17 +7,19 @@ namespace RandomApp.ProductManagement.Domain.Entities
 {
     public class Product
     {
-        public int Id { get; set; }
+        public Guid Id { get; private set; }
 
-        public int OriginalApiId { get; set; }
+        public int OriginalApiId { get; private set; }
 
-        public string Name { get; set; }
+        public string Name { get; private set; } = string.Empty;
 
         public Price Price { get; private set; }
 
-        public string Category { get; set; }
-        public string Description { get; set; }
+        public SKU SKU { get; private set; }
 
-        public string Image { get; set; }
+        public string Category { get; private set; }
+        public string Description { get; private set; }
+
+        public string Image { get; private set; }
     }
 }
---
+++ ProductManagement.Domain/ValueObjects/SKU.cs
@@ -0,0 +1,23 @@
+﻿using RandomApp.ProductManagement.Domain.Exceptions;
+
+namespace RandomApp.ProductManagement.Domain.ValueObjects
+{
+    public record SKU
+    {
+        private const int DefaultLength = 15;
+        private SKU(string value) => Value = value;
+        public string Value { get; private init; }
+
+        // can be only created through the Create method -> forces factory method pattern
+        public static SKU? Create(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new DomainException("Value cannot be null");
+
+            if (value.Length > DefaultLength)
+                throw new DomainException("Length has to be less than 15 characters");
+
+            return new SKU(value);
+        }
+    }
+}
