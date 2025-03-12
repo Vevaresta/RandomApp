b9b502a644574f86303dfb4bf8bf6421789484c6
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-03-12 10:16:39 +0100

    Added Fluent Validation for ShoppingCartDto and ShoppingCartItemDto

---
+++ RandomApp.ShoppingCart.Application/DataTransferObjects/ShoppingCartDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿
+using FluentValidation;
+
+namespace RandomApp.ShoppingCartManagement.Application.DataTransferObjects
+{
+    public class ShoppingCartDtoValidator : AbstractValidator<ShoppingCartDto>
+    {
+        public ShoppingCartDtoValidator()
+        {
+            RuleFor(x => x.UserId).GreaterThan(0).WithMessage("User ID must be a positive number.");
+            RuleFor(x => x.CreatedAt).NotEmpty().WithMessage("Creation date is required.");
+            RuleFor(x => x.Items).NotNull().WithMessage("Items collection cannot be null.");
+            RuleForEach(x => x.Items).SetValidator(new ShoppingCartItemDtoValidator());
+        }
+    }
+}
---
+++ RandomApp.ShoppingCart.Application/DataTransferObjects/ShoppingCartItemDtoValidator.cs
@@ -0,0 +1,16 @@
+﻿
+using FluentValidation;
+
+namespace RandomApp.ShoppingCartManagement.Application.DataTransferObjects
+{
+    public class ShoppingCartItemDtoValidator : AbstractValidator<ShoppingCartItemDto>
+    {
+        public ShoppingCartItemDtoValidator()
+        {
+            RuleFor(x => x.ProductId).GreaterThan(0).WithMessage("Product ID must be a positive number.");
+            RuleFor(x => x.Name).NotEmpty().WithMessage("Product name is required").MaximumLength(256).WithMessage("Product name cannot exceed 256 characters.");
+            RuleFor(x => x.Price).GreaterThanOrEqualTo(0).WithMessage("Price must be a non-negative number.");
+            RuleFor(x => x.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+        }
+    }
+}
