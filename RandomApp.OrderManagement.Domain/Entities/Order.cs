81f2f7f4f6aefa79b6b826824611d4d48a67a8a4
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-03-24 19:11:20 +0100

    Made Order class

---
+++ RandomApp.OrderManagement.Domain/Entities/Order.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomApp.OrderManagement.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; private set; }
+
+        public int CustomerId { get; private set; }
+        public DateTime CreatedAt { get; private set; }
+
+        public DateTime LastModified {  get; private set; }
+        public Enum OrderStatus { get; private set; }
+        private readonly List<OrderItem> _orderItems = new List<OrderItem>();
+
+        public IReadOnlyCollection<OrderItem> OrderItems => _orderItems.AsReadOnly();
+
+        public ShippingAddress ShippingAddress { get; private set; }
+        public BillingAddress BillingAddress { get; private set; }
+        public TotalPrice 
+
+        public Enum PaymentMethod {  get; private set; }
+
+        public Enum PaymentStatus { get; private set; }
+    }
+}
