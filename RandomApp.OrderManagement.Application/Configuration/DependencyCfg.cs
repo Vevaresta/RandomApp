d3acf4a07bf3f9b6580ecef8fda69f9198110dbe
Vevaresta <73753308+Vevaresta@users.noreply.github.com>
2025-04-17 17:10:46 +0200

    Add Config and refactor

---
+++ RandomApp.OrderManagement.Application/Configuration/DependencyCfg.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomApp.OrderManagement.Application.Configuration
+{
+    public static class DependencyCfg
+    {
+    }
+}
